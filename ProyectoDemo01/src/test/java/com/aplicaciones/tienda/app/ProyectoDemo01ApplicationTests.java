7cbeaadbcf17e4e97a08c46d452353e097674e7b
Diego33KX <diegofj203@gmail.com>
2022-05-21 23:45:34 -0500

    Primera Subida

---
+++ ProyectoDemo01/src/test/java/com/aplicaciones/tienda/app/ProyectoDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicaciones.tienda.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
